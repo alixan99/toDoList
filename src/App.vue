e40b477e3233fbf28a84bcd8837e12d7707063dd
Тхатель Алибек <alibek.tkhatel@mail.ru>
2025-02-01 22:16:41 +0300

    add font end css files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div></div>
+<div class="app-container" id="app">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia officiis veritatis vel illo. Tenetur magnam saepe aperiam assumenda necessitatibus modi aspernatur voluptatibus temporibus illum numquam. Quod tenetur inventore omnis eligendi!</div>
 </template>
 
 <script>
@@ -12,5 +12,12 @@ export default {
 </script>
 
 <style lang="scss">
-
+.app-container {
+  background-image: url('@/assets/image/background.jpg');
+  background-size: no-repeat;
+  background-position: center;
+  background-size: cover;
+  height: 100vh;
+  font-family: 'Montserrat-Regular', sans-serif;
+}
 </style>
