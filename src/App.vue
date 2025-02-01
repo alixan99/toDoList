b6593ff07b3a1a482656d0a2348b6f78e6ae9b9b
Тхатель Алибек <alibek.tkhatel@mail.ru>
2025-02-01 22:27:59 +0300

    delete excess text

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="app-container" id="app">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia officiis veritatis vel illo. Tenetur magnam saepe aperiam assumenda necessitatibus modi aspernatur voluptatibus temporibus illum numquam. Quod tenetur inventore omnis eligendi!</div>
+<div class="app-container" id="app"></div>
 </template>
 
 <script>
