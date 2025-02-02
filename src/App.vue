fa5469dad77e9630813d795bbd0aec5777a287d7
Тхатель Алибек <alibek.tkhatel@mail.ru>
2025-02-02 12:25:55 +0300

    fix bag

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-<div class="app-container" id="app"></div>
+  <div class="app-container" id="app">
+    <TodoList />
+  </div>
 </template>
 
 <script>
+import TodoList from '@/components/blocks/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
+    TodoList
   }
 }
 </script>
 
 <style lang="scss">
 .app-container {
-  background-image: url('@/assets/image/background.jpg');
+  background-image: url('@/assets/image/background.jpg');/* подходящий фон */
   background-size: no-repeat;
   background-position: center;
   background-size: cover;
   height: 100vh;
   font-family: 'Montserrat-Regular', sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  height: 100vh; /* на всю высоту экрана */
+  background-color: #f4f4f9;
 }
 </style>
